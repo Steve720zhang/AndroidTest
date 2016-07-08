1e3419877c18b4ed8fc4990f3791af7af4f6acde
zhangzequn <776021361@qq.com>
2016-07-08 17:01:18 +0800

    剔除一切navigator自带的bar相关组件 另采取方法实现

--- app/Navigator/NavigatorMain.js
+++ app/Navigator/NavigatorMain.js
@@ -5,6 +5,8 @@ import React from 'react';
 import {
     StyleSheet,
     View,
+    Text,
+    TouchableOpacity,
     Navigator
 } from 'react-native';
 import FirstPage from '../components/FirstPage';
@@ -16,38 +18,31 @@ class SampleComponent extends React.Component {
         let defaultComponent = FirstPage;
         // 这个组件的Class，用来一会儿实例化成 <Component />标签
         return (
-        <Navigator
-          initialRoute={{ name: defaultName, component: defaultComponent }}
-          //这个指定了默认的页面，也就是启动app之后会看到界面的第一屏。 需要填写两个参数: name 跟 component。
-          configureScene={(route) => {
-            return Navigator.SceneConfigs.FadeAndroid;
-            //HorizontalSwipeJumpFromRight
-            //VerticalDownSwipeJump
-            //HorizontalSwipeJump
-            //FadeAndroid
-            //这个是页面之间跳转时候的动画
-          }}
-          renderScene={(route, navigator) => {
-            let Component = route.component;
-            // 传递navigator 使得下一个页面可以使用。
-            return <Component {...route.params} navigator={navigator} />
-          }} 
-          navigationBar={
-          <Navigator.NavigationBar
-            style={style.navContainer}
-            routeMapper={NavigationBarRouteMapper}/>}
+          <Navigator
+            initialRoute={{ name: defaultName, component: defaultComponent }}
+            //这个指定了默认的页面，也就是启动app之后会看到界面的第一屏。 需要填写两个参数: name 跟 component。
+            configureScene={(route) => {
+              return Navigator.SceneConfigs.FadeAndroid;
+              //HorizontalSwipeJumpFromRight
+              //VerticalDownSwipeJump
+              //HorizontalSwipeJump
+              //FadeAndroid
+              //这个是页面之间跳转时候的动画
+            }}
+            renderScene={(route, navigator) => {
+              let Component = route.component;
+              // 传递navigator 使得下一个页面可以使用。
+              return <Component {...route.params} navigator={navigator} />
+            }} 
+           />
         );
     }
 } 
+
 //navigator的跳转样式总共都有些什么？可查如下：
 //node_modules/react-native/Libraries/CustomComponents/Navigator/NavigatorSceneConfigs.js
 
-const style = StyleSheet.create({
-  navContainer{
-    backgroundColor: '#e5e5e5e',
-    height: 50,
-    justifyContent: 'center',
-  }
+const styles = StyleSheet.create({
 });
 
 export default SampleComponent;
